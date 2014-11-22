7803116e86fb47e0e70407d7a762f4ef47fcf87b
srfrnk <srfrnk@gmail.com>
2014-11-22 12:20:57 +0200

    bug fixes

--- addfile/templates/models/model.js
+++ addfile/templates/models/model.js
@@ -1,6 +1,7 @@
 require("requirejs").define("models/<%=nameCamel%>", ["models/mongooseHelper"], function (mongooseHelper) {
 	var <%=nameCamel%>Schema,<%=nameCapital%>Model,<%=nameCamel%>Model={};
 	mongooseHelper.connected.then(function () {
+        try {
 		<%=nameCamel%>Schema = mongooseHelper.mongoose.Schema({
 			name: String,
 			description: String,
@@ -9,6 +10,10 @@ require("requirejs").define("models/<%=nameCamel%>", ["models/mongooseHelper"], 
 		});
 		<%=nameCapital%>Model = mongooseHelper.mongoose.model("<%=nameCamel%>",<%=nameCamel%>Schema);
 		 <%=nameCamel%>Model.<%=nameCapital%>Model = <%=nameCapital%>Model;
+        }
+        catch (e) {
+            console.log(e, new Error().stack);
+        }
 	});
 	return <%=nameCamel%>Model;
 });
